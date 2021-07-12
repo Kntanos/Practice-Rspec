b969a2c0f167c0877fcaba5c78b03943254c9538
KNtanos <86077849+Kntanos@users.noreply.github.com>
2021-07-12 21:12:31 +0100

    First spec

---
+++ RPS/spec/play_spec.rb
@@ -0,0 +1,20 @@
+require "rps"
+
+describe Game do
+
+    describe ".play" do
+        context do 
+            "when Kostas plays rock and Computer plays scissors"
+            it "should return Kostas won!" do
+                player_1 = Player.new("Kostas")
+                player_2 = Player.new("Computer")
+                game = Game.new(player_1, player_2)
+                
+                allow(player_1).to receive(:choice).and_return("rock")
+                allow(player_2).to receive(:choice).and_return("scissors")
+                
+                expect(game.play()).to eq "Kostas Won!"
+            end
+        end  
+    end
+end
